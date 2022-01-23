e12241226f3aac7e7dde875aa2a488770f931fbc
eparkington <81046295+eparkington@users.noreply.github.com>
2022-01-23 13:42:52 -0500

    async update

--- app.js
+++ app.js
@@ -13,24 +13,21 @@ button.addEventListener('click', async (getSpell) => {
         console.log(err)
         alert('Failed to fetch new spell');
     } */
-    
-    const getSpells = ((data) => {
-        const index = Math.floor(Math.random() * data.results.length);
-        const spellName = data.results[index].name;
-        const spellDesc = data.results[index].desc;
-        const html = `<h2>${spellName}</h2>
-        <p>${spellDesc}`;
-        div.innerHTML = html;
-    });
+    const getSpells = async () => {
+        const response = await fetch('https://api.open5e.com/spells/');
+        const data = await response.json();
+        return data;
+    };
 
-
-//fetch api data
-
-    fetch('https://api.open5e.com/spells/').then((response) => {
-        return response.json();
-    }).then(data => {
-        getSpells(data);
-    }).catch(err => {
+    getSpells()
+        .then( data => {
+            const index = Math.floor(Math.random() * data.results.length);
+            const spellName = data.results[index].name;
+            const spellDesc = data.results[index].desc;
+            const html = `<h2>${spellName}</h2>
+            <p>${spellDesc}`;
+            div.innerHTML = html;
+        }).catch(err => {
         console.log('rejected', err);
     });
 
